46e93594d0a006da6f8dacd8a194150c3cd05d29
muthuram05 <mr032495@gmail.com>
2024-07-13 16:55:59 +0530

    chnages

--- react/src/components/career_card/index.jsx
+++ react/src/components/career_card/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import "./style.css"
 const CareerCard = () => {
   return (
-    <div>
-    <img src="https://i.pinimg.com/736x/06/2b/83/062b83d4e3e10d2968fbf72b14cd24a7.jpg" alt="" />
-    <h3>Bias to Action</h3>
-    <p>Experiment, fail, learn, share, repeat. Seek forgiveness instead of
-    permission.</p>
-  </div>
-  )
-}
+    <div className="career-card">
+      <img
+        src="https://i.pinimg.com/736x/06/2b/83/062b83d4e3e10d2968fbf72b14cd24a7.jpg"
+        alt=""
+        width={"100px"}
+        height={"120px"}
+      />
+      <h3>Bias to Action</h3>
+      <p>
+        Experiment, fail, learn, share, repeat. Seek forgiveness instead of
+        permission.
+      </p>
+    </div>
+  );
+};
 
-export default CareerCard
+export default CareerCard;
